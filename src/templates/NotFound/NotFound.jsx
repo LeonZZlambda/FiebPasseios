ea2874904563917117920d590e6e7edbf5870728
LeonZZlambda <leonzzlopes@proton.me>
2024-09-21 01:39:04 -0300

    fifty-commit

--- src/templates/NotFound/NotFound.jsx
+++ src/templates/NotFound/NotFound.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 import DeadLink from '../../assets/images/404.png'
 
+
 function NotFound() {
   return (
     <div>
         <div className='fieb-logo'>
-            <h2 className='title2'>Ops, parece que você encontrou um link morto :P</h2>
-            <img src={DeadLink} alt="Error404" />
+            <h2 className='titleError'>Ops, parece que você encontrou um link morto :P</h2>
+            <img src={DeadLink} alt="Error404" className='Error'/>
             <h1 className='title1'>404</h1>
 
         </div>
